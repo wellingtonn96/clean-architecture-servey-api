143b1e56c12876d99f7ad226d033558c1ef5d821
wellingtonn96 <weltossousa@gmail.com>
2022-06-26 23:51:50 -0300

    feat: add server node

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, { json } from "express";
+import routes from "./routes";
+import "./database";
+
+const app = express();
+
+app.use(json());
+app.use(routes);
+
+app.listen(3333, () => console.log("server runing on port 3333"));
