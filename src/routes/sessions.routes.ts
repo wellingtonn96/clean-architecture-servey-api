5eea7fd1875c8a9215c04b47cafcfa1cb34e7021
wellingtonn96 <weltossousa@gmail.com>
2022-06-26 23:56:30 -0300

    feat: add route to login and signup

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,27 @@
+import { json, Router } from "express";
+import UserRepository from "../repositories/UserRepository";
+import { AuthenticateUserService } from "../services/AuthenticateUserService";
+
+const sessionRouter = Router();
+
+sessionRouter.post("/", async (request, response) => {
+  try {
+    const data = request.body;
+
+    const authenticateUserService = new AuthenticateUserService(
+      new UserRepository()
+    );
+
+    const authenticateUser = await authenticateUserService.execute({
+      email: data.email,
+      password: data.password,
+    });
+
+    return response.status(200).json({ data: authenticateUser });
+  } catch (error) {
+    //@ts-ignore
+    return response.status(401).json({ err: error.message });
+  }
+});
+
+export { sessionRouter };
