89392104df8f45bb6aba6e316c7b6e8760a564ca
wellingtonn96 <weltossousa@gmail.com>
2022-06-26 23:52:59 -0300

    feat: config  mongodb database

---
+++ src/database/index.ts
@@ -0,0 +1,17 @@
+import { connect, connection } from "mongoose";
+
+export function createConnection(): void {
+  try {
+    connect("mongodb://localhost:27017/survey_db", {
+      //@ts-ignore
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    connection.once("open", () => console.log("database rondando"));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+createConnection();
