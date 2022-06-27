846c9e3bde202112686570e9e622150b3d1d5540
wellingtonn96 <weltossousa@gmail.com>
2022-06-26 23:54:22 -0300

    feat: add Survey repository

---
+++ src/repositories/SurveyRepository.ts
@@ -0,0 +1,81 @@
+import { Model } from "mongoose";
+import { surveyModel, ISurvey } from "../models/Survey";
+
+class SurveyRepository {
+  private surveys: Model<ISurvey>;
+
+  constructor() {
+    this.surveys = surveyModel;
+  }
+
+  public all() {
+    return this.surveys.find();
+  }
+
+  public async findByAnswer(answer: string, id: string) {
+    return this.surveys.findOne({});
+  }
+
+  public async getOne(id: string) {
+    return this.surveys.findOne({
+      _id: id,
+    });
+  }
+
+  public async create(data: ISurvey): Promise<any> {
+    const results = await this.surveys.create({
+      question: data.question,
+      answers: data.answers,
+    });
+
+    return results;
+  }
+
+  public async createVote({ answer, id }: { answer: string; id: string }) {
+    const survey = await this.getOne(id);
+
+    const updateSurvey =
+      survey &&
+      survey.answers.map((item) => {
+        if (item.answer === answer) {
+          return {
+            image: item.image,
+            answer: item.answer,
+            count: item.count + 1,
+            percent: parseInt(
+              (
+                ((item.count + 1) /
+                  survey.answers.reduce((previous, current) => {
+                    return previous + current.count;
+                  }, 1)) *
+                100
+              ).toString()
+            ),
+          };
+        }
+        return {
+          image: item.image,
+          answer: item.answer,
+          count: item.count,
+          percent: parseInt(
+            (
+              (item.count /
+                survey.answers.reduce((previous, current) => {
+                  return previous + current.count;
+                }, 1)) *
+              100
+            ).toString()
+          ),
+        };
+      });
+
+    const surveyUpdated = await this.surveys.updateOne(
+      { _id: id },
+      { $set: { answers: updateSurvey } }
+    );
+
+    return surveyUpdated;
+  }
+}
+
+export default SurveyRepository;
