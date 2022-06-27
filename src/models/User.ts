48d8b3a60743fceb2d54c201a9ae26a597882d29
wellingtonn96 <weltossousa@gmail.com>
2022-06-26 23:54:01 -0300

    feat: add schema User

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+import { Schema, Document, model } from "mongoose";
+
+export interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const UserModel = model("User", UserSchema);
